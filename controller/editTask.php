89f9c178d31cc7c0e17432f20bc95bb8348e39f9
Rookiebutgood <45212062+Rookiebutgood@users.noreply.github.com>
2020-01-20 10:43:23 +0600

    Update editTask.php

--- controller/editTask.php
+++ controller/editTask.php
@@ -1,6 +1,8 @@
 <?php
 include_once('model/index.php');
 
+session_start();
+
 Database::connect();
 
 if($_GET['key']){
@@ -9,25 +11,28 @@ if($_GET['key']){
     $data = $data[0];
 }
 
-if($_POST){
-    //Получаем текст задачи из базы и из формы
-    $newTask = $_POST['task'];
-    $oldTask = $data['task'];
+if($_POST && $_SESSION['role'] == 'admin'){
+  //Получаем текст задачи из базы и из формы
+  $newTask = $_POST['task'];
+  $oldTask = $data['task'];
+
+  //Если они различаются, то пишем в базу текст с формы
+  if($newTask != $oldTask ){
+    $changed = 1;
+    Database::change("UPDATE tasks SET task ='$newTask', changed ='$changed', status = '$status' WHERE number=".$key);
+  }
+
+  //Проверяем значение чекбокса выполнено, пишем его состояние в базу
+  if(isset($_POST['status'])){
+    Database::change("UPDATE tasks SET status = 1 WHERE number=".$key);
+  }else{
+    Database::change("UPDATE tasks SET status = 0 WHERE number=".$key);
+  }
+
+  header("Location: /");
+
+}elseif($_POST){
     
-    //Если они различаются, то пишем в базу текст с формы
-    if($newTask != $oldTask ){
-        $changed = 1;
-        Database::change("UPDATE tasks SET task ='$newTask', changed ='$changed', status = '$status' WHERE number=".$key);
-    }
-
-    //Проверяем значение чекбокса выполнено, пишем его состояние в базу
-    if(isset($_POST['status'])){
-      Database::change("UPDATE tasks SET status = 1 WHERE number=".$key);
-    }else{
-      Database::change("UPDATE tasks SET status = 0 WHERE number=".$key);
-    }
-
-    header("Location: /");
+  header("Location: /login");
+    
 }
-
-?>
